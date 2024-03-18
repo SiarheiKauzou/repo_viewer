656499b5fd9a99fcfa778ad985e9aff5491d5002
siarhei_kauzou <sergei@clearbluedesign.com>
2024-03-18 18:36:02 +0300

    handling the authorization response

--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -1,6 +1,10 @@
+import 'dart:ffi';
+
+import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 import 'package:oauth2/oauth2.dart';
+import 'package:repo_viewer/auth/domain/auth_failure.dart';
 import 'package:repo_viewer/auth/infrastructure/credentials_storage/credentials_storage.dart';
 import 'package:http/http.dart' as http;
 
@@ -47,15 +51,34 @@ class GithubAuthenticator {
 
   AuthorizationCodeGrant createGrant(Credentials credentials) {
     return AuthorizationCodeGrant(
-        clientId,
-        authorizationEndpoint,
-        tokenEndpoint,
-        secret: clientSecret,
-        httpClient: GithubHttpClient(),
-      );
+      clientId,
+      authorizationEndpoint,
+      tokenEndpoint,
+      secret: clientSecret,
+      httpClient: GithubHttpClient(),
+    );
   }
 
   Uri getAuthorizeUri(AuthorizationCodeGrant grant) {
     return grant.getAuthorizationUrl(redirectUri, scopes: scopes);
+  }
+
+  Future<Either<AuthFailure, Unit>> handlerAuthorizationResponse({
+    required AuthorizationCodeGrant grant,
+    required Map<String, String> query,
+  }) async {
+    try {
+      final httpClient = await grant.handleAuthorizationResponse(query);
+
+      await _credentialsStorage.save(httpClient.credentials);
+
+      return right(unit);
+    } on FormatException {
+      return left(const AuthFailure.storage());
+    } on AuthorizationException catch (e) {
+      return left(AuthFailure.server('${e.error}: ${e.description}'));
+    } on PlatformException {
+      return left(const AuthFailure.storage());
+    }
   }
 }
