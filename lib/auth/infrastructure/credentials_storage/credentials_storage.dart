313e33623eb17fa99fe665227f472eaedd67f0be
siarhei_kauzou <sergei@clearbluedesign.com>
2024-03-18 14:05:29 +0300

    secure credentials storage

--- lib/auth/infrastructure/credentials_storage/credentials_storage.dart
+++ lib/auth/infrastructure/credentials_storage/credentials_storage.dart
@@ -3,7 +3,7 @@ import 'package:oauth2/oauth2.dart';
 abstract class CredentialsStorage {
   Future<Credentials?> read();
 
-  Future<Credentials?> save(Credentials credentials);
+  Future<void> save(Credentials credentials);
 
   Future<void> clear();
 }
--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -1,4 +1,5 @@
 import 'package:oauth2/oauth2.dart';
+import 'package:repo_viewer/auth/infrastructure/credentials_storage/credentials_storage.dart';
 
 class GithubAuthenticator {
   Future<Credentials> getSignedInCredentials() async {
