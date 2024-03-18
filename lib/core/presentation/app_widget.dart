6ec4ff9a90e2b7e8d33d4671440f210e9895f7e8
siarhei_kauzou <sergei@clearbluedesign.com>
2024-03-19 00:58:21 +0300

    initialization provider

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -1,26 +1,30 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/auth/shared/providers.dart';
 import 'package:repo_viewer/core/presentation/routes/app_router.dart';
 
-class AppWidget extends StatefulWidget {
+final initializationProvider = FutureProvider<Unit>((ref) async {
+  final authNotifier = ref.read(authNotifierProvider.notifier);
+  await authNotifier.checkAndUpdateAuthenticationStatus();
+  return unit;
+});
+
+class AppWidget extends ConsumerWidget {
   const AppWidget({super.key});
 
-  @override
-  State<AppWidget> createState() => _AppWidgetState();
-}
+  static final _appRouter = AppRouter();
 
-class _AppWidgetState extends State<AppWidget> {
-  late final AppRouter _appRouter;
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(
+      initializationProvider,
+      (_, __) {},
+    );
+
     return MaterialApp.router(
       title: 'Repo Viewer',
       routerConfig: _appRouter.config(),
     );
-  }
-
-  @override
-  void initState() {
-    _appRouter = AppRouter();
-    super.initState();
   }
 }
