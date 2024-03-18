d5e37afb6b310c79529e82929ce579d9ac1314c5
siarhei_kauzou <sergei@clearbluedesign.com>
2024-03-18 13:40:01 +0300

    starting with the auth infrastructure layer

---
+++ lib/auth/infrastructure/credentials_storage/credentials_storage.dart
@@ -0,0 +1,9 @@
+import 'package:oauth2/oauth2.dart';
+
+abstract class CredentialsStorage {
+  Future<Credentials?> read();
+
+  Future<Credentials?> save(Credentials credentials);
+
+  Future<void> clear();
+}
