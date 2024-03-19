c82080709004bdec2f469b9a9d533c20119e1704
siarhei_kauzou <sergei@clearbluedesign.com>
2024-03-19 20:22:00 +0300

    intercept redirection attempts & finishing webview

--- lib/auth/presentation/authorization_page.dart
+++ lib/auth/presentation/authorization_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 @RoutePage()
@@ -32,8 +33,20 @@ class _AuthorizationPageState extends State<AuthorizationPage> {
 
   @override
   void initState() {
+    WebViewCookieManager().clearCookies();
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(NavigationDelegate(
+        onNavigationRequest: (request) {
+          if (request.url
+              .startsWith(GithubAuthenticator.redirectUri.toString())) {
+            widget.onAuthorizationCodeRedirectAttemp(Uri.parse(request.url));
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ))
+      ..clearCache()
       ..loadRequest(widget.authorizationUri);
     super.initState();
   }
