d429352b995ba0619ec7578d8cca9bd9c3cab9fb
siarhei_kauzou <sergei@clearbluedesign.com>
2024-03-20 01:51:27 +0300

    creating a remoteresponse union

---
+++ lib/github/repos/starred_repos/infrastucture/starred_repos_remote_service.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:repo_viewer/core/infrastructure/remote_response.dart';
+import 'package:repo_viewer/github/repos/core/infrastucture/github_repo_dto.dart';
+
+class StarredReposRemoteService {
+  const StarredReposRemoteService(this._dio);
+
+  final Dio _dio;
+
+  Future<RemoteResponse<List<GithubRepoDTO>>> getStarredReposPage(int page) async {
+ 
+  }
+}
