ae86cec4d8536b503d55a4602c94a0f0ef79ce95
siarhei_kauzou <sergei@clearbluedesign.com>
2024-03-23 13:47:56 +0300

    oauth2 interceptor part 2

--- lib/auth/infrastructure/oauth2_interceptor.dart
+++ lib/auth/infrastructure/oauth2_interceptor.dart
@@ -1,10 +1,17 @@
 import 'package:dio/dio.dart';
+import 'package:repo_viewer/auth/application/auth_notifier.dart';
 import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
 
 class Oauth2Interceptor extends Interceptor {
-  const Oauth2Interceptor(this._authenticator);
+  const Oauth2Interceptor(
+    this._authenticator,
+    this._authNotifier,
+    this._dio,
+  );
 
   final GithubAuthenticator _authenticator;
+  final AuthNotifier _authNotifier;
+  final Dio _dio;
 
   @override
   Future<void> onRequest(
@@ -28,7 +35,23 @@ class Oauth2Interceptor extends Interceptor {
       final credentials = await _authenticator.getSignedInCredentials();
       credentials != null && credentials.canRefresh
           ? await _authenticator.refresh(credentials)
-          : await _authenticator.signOut();
+          : await _authenticator.clearCredentialsStorage();
+
+      await _authNotifier.checkAndUpdateAuthenticationStatus();
+
+      final refreshCredentials = await _authenticator.getSignedInCredentials();
+
+      if (refreshCredentials != null) {
+        handler.resolve(
+          await _dio.fetch(
+            errorResponse.requestOptions
+              ..headers['Authorization'] =
+                  'bearer ${refreshCredentials.accessToken}',
+          ),
+        );
+      }
+    } else {
+      handler.next(err);
     }
   }
 }
